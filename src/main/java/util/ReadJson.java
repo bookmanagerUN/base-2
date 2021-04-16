3f729c0b6a6f5787ddf9064d185fa2187ff86d32
daniel7985 <67341875+daniel7985@users.noreply.github.com>
2021-04-16 13:42:26 -0500

    Update ReadJson.java
    el metodo devuelve un arr o arraylist

--- src/main/java/util/ReadJson.java
+++ src/main/java/util/ReadJson.java
@@ -10,5 +10,27 @@ package util;
  * @author Usuario
  */
 public class ReadJson {
-    
+    //Trae un arraylist
+    /*public List<AuxBook> readJson() throws FileNotFoundException {
+        // LEER JSON
+        Reader reader = new FileReader("LibrosPrueba.json");
+
+        Type type = new TypeToken<Collection<AuxBook>>() {
+        }.getType();
+
+
+        List<AuxBook> result = WriteJson.gson.fromJson(reader, type);
+        return result;
+    }*/
+
+    //Trae un arr
+    public AuxBook[] readJson() throws FileNotFoundException {
+        // LEER JSON
+        Reader reader = new FileReader("LibrosPrueba.json");
+
+        /* Type type = new TypeToken<Collection<AuxBook>>() {
+        }.getType();*/
+
+        return  WriteJson.gson.fromJson(reader, AuxBook[].class);
+    }
 }
