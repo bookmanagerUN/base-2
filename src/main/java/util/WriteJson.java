a605e686331e044d18f141d691d29a9027e4dc7c
daniel7985 <67341875+daniel7985@users.noreply.github.com>
2021-04-16 13:41:30 -0500

    Update WriteJson.java
    clase writer añadida se crea el Json apartir de una Linked list de books

--- src/main/java/util/WriteJson.java
+++ src/main/java/util/WriteJson.java
@@ -10,5 +10,127 @@ package util;
  * @author Usuario
  */
 public class WriteJson {
+    private AuxBook[] arrBook;
+
+    private  MyBooks MyBook;
+
+
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public void createAuxBook(LinkedList<Book> myBook) throws IOException {
+        int actualPage  ;
+        boolean status  ;
+        Object[] bookInformation;
+        Notes[] notes ;
+        int size;
+        size= myBook.count;
+        Node<Book> aux =myBook.head;
+        arrBook = new AuxBook[size];
+
+        for(int i=0; i<size;i++){
+
+            actualPage = aux.data.getActualPage();
+            status = aux.data.getStatus();
+            bookInformation = informationToArr(aux.data.getBookInformation().bookInformation()) ;
+            notes = notesToArr(aux.data.getNotes().getBookNotes());
+
+            AuxBook auxBook = new AuxBook(actualPage,status,bookInformation,notes);
+            arrBook[i] = auxBook;
+        }
+        createJson(arrBook);
+
+
+
+    }
+    public void createJson(AuxBook[] auxBooks) throws IOException {
+        //CREAR JSON
+
+        Writer writer = new FileWriter("LibrosPrueba.json");
+        writer.write(gson.toJson(arrBook));
+        writer.close();
+    }
+
+    public void crearjson(LinkedList<Book> myBook){
+
+    }
+    public Object[] informationToArr(LinkedList<Object> bookInformation){
+        Node<Object> aux = bookInformation.head;
+        int size2 = bookInformation.count;
+        Object[] arr = new Object[size2];
+        int i=0;
+        while (aux != null){
+            arr[i] = aux.data;
+            i +=1;
+            aux = aux.next;
+        }
+        return arr;
+
+
+    }
+    public Notes[] notesToArr(LinkedList<Notes> bookNotes){
+        Node<Notes> aux = bookNotes.head;
+        int size2 = bookNotes.count;
+        Notes[] arr = new Notes[size2];
+        int i=0;
+        while (aux != null){
+            arr[i] = aux.data;
+            i +=1;
+            aux = aux.next;
+        }
+        return arr;
+    }
     
+}
+class AuxBook {
+    private  int actualPage  ;
+    private  boolean status  ;
+    private  Object[] bookInformation;
+    private  Notes[] notes ;
+
+    public AuxBook(int actualPage, boolean status, Object[] bookInformation, Notes[] notes) {
+        this.actualPage = actualPage;
+        this.status = status;
+        this.bookInformation = bookInformation;
+        this.notes = notes;
+    }
+
+    public int getActualPage() {
+        return actualPage;
+    }
+
+    public void setActualPage(int actualPage) {
+        this.actualPage = actualPage;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Object[] getBookInformation() {
+        return bookInformation;
+    }
+    public void setBookInformation(Object[] bookInformation) {
+        this.bookInformation = bookInformation;
+    }
+    public Notes[] getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes[] notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public String toString() {
+        return "AuxBook{" +
+                "actualPage=" + actualPage +
+                ", status=" + status +
+                ", bookInformation=" + Arrays.toString(bookInformation) +
+                ", notes=" + Arrays.toString(notes) +
+                '}';
+    }
 }
